93dfcd82c4e5d21561308c536efe8bbcc47c4fb3
Enrico Renna <renna.e@outlook.com>
2022-04-11 21:42:07 +0100

    Various improvements

--- js/play.js
+++ js/play.js
@@ -1,8 +1,5 @@
 const question = document.getElementById("question");
-const ans_one = document.getElementById("1");
-const ans_two = document.getElementById("2");
-const ans_three = document.getElementById("3");
-const ans_four = document.getElementById("4");
+const answer = [document.getElementById("1"), document.getElementById("2"), document.getElementById("3"), document.getElementById("4")];
 
 let score = 0;
 let answered = 0;
@@ -12,40 +9,60 @@ let toAnswer = [];
 const MAX_TIME = 600; // Seconds
 const MAX_QUESTIONS = 3; // For testing reason, otherwise: localStorage.getItem("number");
 
+let example_array = [];
 
-function play() {
+
+
+async function play() {
     /* Set previous values to 0 */
     score = 0;
     answered = 0;
     timeElapsed = 0;
 
-    start("sample.json");
+    example_array = await getQuestions("sample.json");
+    console.log(example_array);
+    loadQuestion(example_array);
 }
 
-function loadQuestion(data) {
+function loadQuestion(array) {
+    data = array[answered];
     /* Display Question & Answers */
     question.innerHTML = data.question;
-    ans_one.innerHTML = data.correct_answer;
-    ans_two.innerHTML = data.incorrect_answers[0];
-    ans_three.innerHTML = data.incorrect_answers[1];
-    ans_four.innerHTML = data.incorrect_answers[2];
+    /* Randomise Questions Order */
+    let answers = [];
+    answers.push(data.correct_answer);
+
+    for(let loop = 0; loop < 3; loop++) {
+        answers.push(data.incorrect_answers[loop]);
+    }
+
+    shuffleArray(answers);
+
+
+    for(let loop = 0; loop < 4; loop++) {
+        answer[loop].innerHTML = answers[loop];
+    }
 
     answered++;
 }
 
 /* Fetch from TriviaDB */
-function start(api_url) {
-    fetch(api_url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            loadQuestion(data.results[answered]);
-            return data.results;
-        })
-        .catch(error => {
-            console.log(error);
-        });
+async function getQuestions(api_url) {
+    try {
+        const data = await fetch(api_url);
+        const json = await data.json();
+        return json.results;
+    } catch(err) {
+        console.log(err);
+    }
 }
 
+
+/* Utilities */
+function shuffleArray(arr) {
+    arr.sort(() => Math.random() - 0.5);
+}
+
+
+/* Run Game */
 play();
